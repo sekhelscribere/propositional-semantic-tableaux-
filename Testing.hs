1836bef214ff9d19d3758d135ede23fe07d03abb
Daniil Khaitovich <d.khaitovich@uva.nl>
2025-01-09 15:55:27 +0100

    tests for parser and semantic tableaux prover

---
+++ Testing.hs
@@ -0,0 +1,27 @@
+module Testing where
+
+import Parser
+import Data.List
+import Test.QuickCheck
+import Learn
+
+
+instance Arbitrary Form where
+    arbitrary = sized randomForm where
+        randomForm :: Int -> Gen Form
+        randomForm 0 = P <$> elements [1,2,3]
+        randomForm n = oneof
+            [ P <$> elements [1,2,3]
+            , Neg <$> randomForm (n `div` 2)
+            , Conj <$> randomForm (n `div` 2) <*> randomForm (n `div` 2) 
+            , Disj <$> randomForm (n `div` 2) <*> randomForm (n `div` 2) ]
+
+
+testTabl = quickCheck (\x -> isSat (Neg x)==(not . isValid) x)
+testParser = quickCheck (\x -> (parsing . show) x==Just x)
+
+
+
+
+
+
